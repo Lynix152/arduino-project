6f5e7f23c6f1c39710d306966d6926bdd4bc9e24
mathias <Pfannenkuchen107@gmx.de>
2023-12-19 20:35:05 +0100

    small changes

--- summary/rtcservoled.ino
+++ summary/rtcservoled.ino
@@ -62,33 +62,13 @@ void loop () {
    
 
     if (now.minute() == 44) {
-      rainbow();
 
       for (int schlag = 0; schlag < 1; schlag = schlag + 1) {
         servoBing();
-        
       }
     }
     
 
-}
-
-void rtcTimeDate() {
-    DateTime now = rtc.now();
-    Serial.print("Date & Time: ");
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.day(), DEC);
-    Serial.print(" (");
-    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-    Serial.print(") ");
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.println(now.second(), DEC);
 }
 
 void servoBing() {
@@ -106,4 +86,30 @@ void rainbow() {
  // Geschwindigkeit und Helligkeit anpassen
   EVERY_N_MILLISECONDS(20) { hue += 1; }
   EVERY_N_MILLISECONDS(10) { FastLED.setBrightness(100); }
+}
+
+void pattern() {
+    // Warte eine Stunde und 15 Minuten vor Beginn des Zyklus
+    delay(4500000);
+
+    // Setze jede Stunde 3 LEDs auf eine Farbe, beginnend mit der vierten LED
+    for (int i = 3; i < NUM_LEDS; i += 8) {
+        leds[i] = CRGB::Red;    // Ändere die Farbe nach Bedarf
+        leds[i + 1] = CRGB::Green;
+        leds[i + 2] = CRGB::Blue;
+    }
+
+    FastLED.show();  // Aktualisiere den LED-Stripe
+    delay(3600000);   // Warte für eine Stunde
+
+    // Schalte die fünf LEDs zwischen den leuchtenden LEDs aus
+    for (int i = 7; i < NUM_LEDS; i += 8) {
+        leds[i] = CRGB::Black;  // Schwarz entspricht ausgeschaltet
+        leds[i + 3] = CRGB::Black;
+        leds[i + 4] = CRGB::Black;
+        leds[i + 5] = CRGB::Black;
+        leds[i + 6] = CRGB::Black;
+    }
+
+    FastLED.show();  // Aktualisiere den LED-Stripe
 }
