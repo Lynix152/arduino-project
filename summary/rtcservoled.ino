6c17241cd3fba46b048f36598924b9537ea34cc5
mathias <Pfannenkuchen107@gmx.de>
2023-12-19 21:00:55 +0100

    small changes

--- summary/rtcservoled.ino
+++ summary/rtcservoled.ino
@@ -58,7 +58,7 @@ void loop () {
 
     delay(1000); // delay 1 seconds
 
-    servoBing();
+    ledstrip()
    
 
     if (now.minute() == 44) {
@@ -69,6 +69,24 @@ void loop () {
     }
     
 
+}
+
+void rtcTimeDate() {
+    DateTime now = rtc.now();
+    Serial.print("Date & Time: ");
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.println(now.second(), DEC);
 }
 
 void servoBing() {
@@ -88,12 +106,23 @@ void rainbow() {
   EVERY_N_MILLISECONDS(10) { FastLED.setBrightness(100); }
 }
 
-void pattern() {
-    // Warte eine Stunde und 15 Minuten vor Beginn des Zyklus
-    delay(4500000);
+
+
+
+void ledstrip() {
+    // Warte bis eine volle Stunde erreicht ist
+    DateTime now = rtc.now();
+    DateTime nextHour = DateTime(now.year(), now.month(), now.day(), now.hour() + 1, 0, 0);
+
+    while (rtc.now() < nextHour) {
+        // other task possible
+    }
+
+    // Bestimme die Position für die LEDs basierend auf der Stunde
+    int hourPosition = (now.hour() - 1) % 12;  // Subtrahiere 1, um 0-11 zu bekommen
 
     // Setze jede Stunde 3 LEDs auf eine Farbe, beginnend mit der vierten LED
-    for (int i = 3; i < NUM_LEDS; i += 8) {
+    for (int i = 3 + hourPosition * 8; i < NUM_LEDS; i += 12) {
         leds[i] = CRGB::Red;    // Ändere die Farbe nach Bedarf
         leds[i + 1] = CRGB::Green;
         leds[i + 2] = CRGB::Blue;
@@ -103,7 +132,7 @@ void pattern() {
     delay(3600000);   // Warte für eine Stunde
 
     // Schalte die fünf LEDs zwischen den leuchtenden LEDs aus
-    for (int i = 7; i < NUM_LEDS; i += 8) {
+    for (int i = 7 + hourPosition * 8; i < NUM_LEDS; i += 12) {
         leds[i] = CRGB::Black;  // Schwarz entspricht ausgeschaltet
         leds[i + 3] = CRGB::Black;
         leds[i + 4] = CRGB::Black;
